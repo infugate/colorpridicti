b3e4fcd78a9fd82d74b356e9d025352de2190713
infugate <infugate43@gmail.com>
2025-03-12 21:40:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ const authRoutes = require('./routes/auth');
 require('dotenv').config();
 const adminRoutes = require('./controller/adminController');
 const app = express();
-// app.use(
-//   cors({
-//     origin: ["https://colorpridiction-front.vercel.app"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
-//     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
-//     credentials: true, // Allow credentials like cookies to be sent
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://colorpridiction-front.vercel.app"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
+    methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
+    credentials: true, // Allow credentials like cookies to be sent
+  })
+);
+// app.use(cors());
 app.use(express.json());
 const colorPredictionRoutes = require('./routes/colorRoutes');
 mongoose.connect(process.env.MONGO_URI, {
